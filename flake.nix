595ad32db1f6d0af5a8971f6f7bd67cadf0239b3
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-13 20:08:45 +0100

    re-add nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "Fish support for the nix-shell environment of the Nix package manager";
 
+  inputs ={
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
   outputs = {nixpkgs, ...}: {
     fish-nix-shell = ./default.nix nixpkgs;
   };
